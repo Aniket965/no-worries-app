75037eba8c5e42e1c5ab42d446d6af8973a1c4b1
aniket sharma <aniket965.as@gmail.com>
2017-03-12 13:06:46 +0530

    added Messaging basic basics

--- app/src/main/java/com/noworries/scibots/noworries/MainActivity.java
+++ app/src/main/java/com/noworries/scibots/noworries/MainActivity.java
@@ -8,22 +8,37 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static android.R.attr.id;
+import static android.R.id.message;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    String ol;
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        et = (EditText) findViewById(R.id.editText);
+
     }
+
+    String phoneNo = "9899023974";
 
     public void startService(View view) {
 //        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -34,9 +49,43 @@ public class MainActivity extends AppCompatActivity {
 //        Location l1 = (Location)  locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 //        Log.d("",l1.toString());
 //        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        ol = et.getText().toString();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
 
-        Intent ss = new Intent(this,BackToWork.class);
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, ol, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.",
+                    Toast.LENGTH_LONG).show();
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
+        }
+
+
+        Intent ss = new Intent(this, BackToWork.class);
         startService(ss);
 
 
-}}
+    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+//        switch (requestCode) {
+//            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    SmsManager smsManager = SmsManager.getDefault();
+//                    smsManager.sendTextMessage(phoneNo, null, ol, null, null);
+//                    Toast.makeText(getApplicationContext(), "SMS sent.",
+//                            Toast.LENGTH_LONG).show();
+//                } else {
+//                    Toast.makeText(getApplicationContext(),
+//                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//            }
+//        }
+
+
+    }
